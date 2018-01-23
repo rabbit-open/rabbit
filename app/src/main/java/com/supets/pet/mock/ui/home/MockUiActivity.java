a7b368f397fa28a9697461646599a24d247bd8ce
B.S.Lee <254608684@qq.com>
2018-01-23 14:42:51 +0800

    init

--- app/src/main/java/com/supets/pet/mock/ui/home/MockUiActivity.java
+++ app/src/main/java/com/supets/pet/mock/ui/home/MockUiActivity.java
@@ -1,7 +1,15 @@
 package com.supets.pet.mock.ui.home;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.annotation.Nullable;
+
+import com.supets.pet.alert.PullMsgService;
+
 
 public class MockUiActivity extends TabLayoutBottomActivity {
 
@@ -9,5 +17,29 @@ public class MockUiActivity extends TabLayoutBottomActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         update();
+        startMsgService();
     }
+
+    private void startMsgService() {
+        Intent intent = new Intent(this, PullMsgService.class);
+        // bindService(intent, msgconnect, Context.BIND_AUTO_CREATE);
+        startService(intent);
+    }
+
+
+    private void stopMsgService() {
+        unbindService(msgconnect);
+    }
+
+    private ServiceConnection msgconnect = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
 }
