71a9f92fdab49ec5189a842175584dea30644c7a
B.S.Lee <254608684@qq.com>
2018-01-23 10:51:46 +0800

    init

--- app/src/main/java/com/supets/pet/mock/ui/home/MockUiActivity.java
+++ app/src/main/java/com/supets/pet/mock/ui/home/MockUiActivity.java
@@ -5,7 +5,6 @@ import android.support.annotation.Nullable;
 
 public class MockUiActivity extends TabLayoutBottomActivity {
 
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
