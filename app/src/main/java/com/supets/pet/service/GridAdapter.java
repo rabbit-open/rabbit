319a1cd53e8df98f1b3318fdde6aa496f09cb98f
B.S.Lee <254608684@qq.com>
2018-05-26 11:27:57 +0800

    优化提示助手

--- app/src/main/java/com/supets/pet/service/GridAdapter.java
+++ app/src/main/java/com/supets/pet/service/GridAdapter.java
@@ -21,9 +21,8 @@ public class GridAdapter extends RecyclerView.Adapter {
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View textView = LayoutInflater.from(parent.getContext()).inflate(R.layout.grid_item, parent, false);
-        TextView label = textView.findViewById(R.id.label);
         TextView name = textView.findViewById(R.id.name);
-        label.setOnClickListener(view -> {
+        textView.setOnClickListener(view -> {
             name.setVisibility(name.getVisibility() == View.GONE ? View.VISIBLE : View.GONE);
         });
         return new RecyclerView.ViewHolder(textView) {
