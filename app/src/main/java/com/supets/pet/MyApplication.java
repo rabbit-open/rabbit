ce868cd1a32decccf3c61b12209c6200ea3c31fb
B.S.Lee <254608684@qq.com>
2018-10-11 15:24:30 +0800

    接口实现库

--- app/src/main/java/com/supets/pet/MyApplication.java
+++ app/src/main/java/com/supets/pet/MyApplication.java
@@ -1,6 +1,8 @@
 package com.supets.pet;
 
 import android.app.Application;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 
 import com.zhy.http.okhttp.OkHttpUtils;
 
@@ -13,4 +15,19 @@ public class MyApplication extends Application {
         super.onCreate();
         OkHttpUtils.initClient(new OkHttpClient());
     }
+
+//    @Override
+//    public Resources getResources() { // 禁止APP字体跟随系统字体放大缩小
+//        Resources res = super.getResources();
+//        Configuration config = new Configuration();
+//        config.setToDefaults();
+//        config.densityDpi=320;
+//        config.fontScale=1f;
+//        try {
+//            res.updateConfiguration(config, res.getDisplayMetrics());
+//        } catch (Exception ignore) {
+//            ignore.printStackTrace();
+//        }
+//        return res;
+//    }
 }
