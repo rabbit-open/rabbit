992e7d7f6cc7d12971afac8a903b1268b7fd3b83
B.S.Lee <254608684@qq.com>
2018-11-10 11:44:54 +0800

    添加日志服务

--- app/src/main/java/com/supets/pet/MyApplication.java
+++ app/src/main/java/com/supets/pet/MyApplication.java
@@ -1,9 +1,9 @@
 package com.supets.pet;
 
 import android.app.Application;
-import android.content.res.Configuration;
-import android.content.res.Resources;
+import android.content.Intent;
 
+import com.supets.pet.service.LogService;
 import com.zhy.http.okhttp.OkHttpUtils;
 
 import okhttp3.OkHttpClient;
@@ -14,20 +14,7 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         OkHttpUtils.initClient(new OkHttpClient());
+        startService(new Intent(this, LogService.class));
     }
 
-//    @Override
-//    public Resources getResources() { // 禁止APP字体跟随系统字体放大缩小
-//        Resources res = super.getResources();
-//        Configuration config = new Configuration();
-//        config.setToDefaults();
-//        config.densityDpi=320;
-//        config.fontScale=1f;
-//        try {
-//            res.updateConfiguration(config, res.getDisplayMetrics());
-//        } catch (Exception ignore) {
-//            ignore.printStackTrace();
-//        }
-//        return res;
-//    }
 }
