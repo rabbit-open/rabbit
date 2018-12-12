bf0e1cb54bcc5d4d177fc5e562c9836281e88937
B.S.Lee <254608684@qq.com>
2018-12-12 15:55:23 +0800

    添加获取手机联系人

--- libweb/src/main/java/com/hualala/libweb/jsbridge/ICaidanmaoJsCallBack.java
+++ libweb/src/main/java/com/hualala/libweb/jsbridge/ICaidanmaoJsCallBack.java
@@ -2,18 +2,6 @@ package com.hualala.libweb.jsbridge;
 
 public interface ICaidanmaoJsCallBack {
 
-    String JSObjectName="webCallCdmInterface";
+    String JSObjectName = "webCallCdmInterface";
 
-    //下单
-    void placeAnOrder();
-
-    //结账
-    void payOrder();
-
-    //是否显示语音按钮
-    //show：是否显示，true为显示，false为隐藏
-    //x,y为按钮左上角在屏幕中的坐标点，当不显示时，x,y传0
-    //width:按钮的宽度，当不显示时，传0
-    //height:按钮的高度，当不显示时，传0
-    void isShowVoiceButton(boolean show, int x, int y, int width, int height);
 }
--- libweb/src/main/java/com/hualala/libweb/jsbridge/ICaidanmaoWebViewJS.java
+++ libweb/src/main/java/com/hualala/libweb/jsbridge/ICaidanmaoWebViewJS.java
@@ -1,11 +1,5 @@
 package com.hualala.libweb.jsbridge;
 
-
-import com.tencent.smtt.sdk.WebView;
-
 public interface ICaidanmaoWebViewJS {
-
-    //语音识别结果传递
-    void inputByVoice(WebView webView, String json);
 
 }
