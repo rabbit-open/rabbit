632098a2b47b4c78a8f9a13568ec4a752a4a6b95
B.S.Lee <254608684@qq.com>
2018-12-25 14:58:24 +0800

    realm测试

--- librealm/src/main/java/com/hualala/librealm/MainActivity.java
+++ librealm/src/main/java/com/hualala/librealm/MainActivity.java
@@ -1,18 +1,11 @@
 package com.hualala.librealm;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 import com.hualala.librealm.dao.DogDao;
-import com.hualala.librealm.model.Dog;
-import com.hualala.librealm.model.DogEmail;
-
-import java.util.List;
-
-import io.realm.RealmList;
+import com.hualala.librealm.utils.RegexUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,28 +15,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(this, RealmActivity.class);
-
-        Dog dog = new Dog();
-        dog.name = "lhj2";
-        dog.age = 3300;
-        dog.emails = new RealmList<>();
-
-        DogEmail email = new DogEmail();
-        email.address = "test2";
-        dog.emails.add(email);
-
-        dao.addDog2(dog);
-
-        dao.update("lhj");
-
-        List<Dog> da=dao.queryAllDog();
-        Log.v("da:", da.size() + "");
-        Log.v("da:", da.get(0).emails.size() + "");
+//        Intent intent = new Intent(this, RealmActivity.class);
+//
+//        Dog dog = new Dog();
+//        dog.name = "lhj2";
+//        dog.age = 3300;
+//        dog.emails = new RealmList<>();
+//
+//        DogEmail email = new DogEmail();
+//        email.address = "test2";
+//        dog.emails.add(email);
+//
+//        dao.addDog2(dog);
+//
+//        dao.update("lhj");
+//
+//        List<Dog> da=dao.queryAllDog();
+//        Log.v("da:", da.size() + "");
+//        Log.v("da:", da.get(0).emails.size() + "");
 
         //intent.putExtra("dogs", dog);
         //startActivity(intent);
+        RegexUtils.find("[\\d.]+", "3422错误3.40");
+        RegexUtils.find("[\\d.]+", "34.22错误3.40");
+        RegexUtils.find("[\\d.]+", "错误");
     }
+
 
     @Override
     protected void onDestroy() {
